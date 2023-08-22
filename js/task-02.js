b11b5b35c56fd7823ebd8dd7cb128e99d39f300c
RushVita <129496341+RushVita@users.noreply.github.com>
2023-08-22 18:09:54 +0300

    task 1-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const list = document.querySelector("#ingredients");
+
+
+for (let i = 0; i < ingredients.length; i++) {
+  const item = document.createElement("li");
+  list.append(item)
+  item.classList.add('item')
+  item.append(ingredients[i])
+}
